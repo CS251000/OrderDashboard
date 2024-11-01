6c1efa31c00400e09fddf5b3c2680462a99e5fdb
chirag <chiragsinghal04@gmail.com>
2024-11-01 17:13:10 +0530

    cc

--- app/api/txtlocalcron/route.js
+++ app/api/txtlocalcron/route.js
@@ -1,17 +1,34 @@
-// app/api/cron/route.js
 import { NextResponse } from 'next/server';
+// import { PrismaClient } from '@prisma/client';
 
-export async function GET() {
+// const prisma = new PrismaClient();
+
+export async function GET(req) {
   const apiKey = process.env.TXT_API_KEY;
-  const numbers = "9818907290"; 
+  numbers="9818907290";
   const sender = "MYNACH";
   const message = "Hello Parents, Sunday Gita Class - 3 (Doing Work, Without Desire) Starts in 1 Hour on Zoom by myNachiketa.com. Meeting Link: https://bit.ly/4bTqSux";
 
-  
-  const url = `https://api.textlocal.in/send/?apiKey=${encodeURIComponent(apiKey)}&numbers=${encodeURIComponent(numbers)}&sender=${encodeURIComponent(sender)}&message=${encodeURIComponent(message)}`;
 
   try {
-    const response = await fetch(url, { method: "POST" }); 
+//     const orders = await prisma.order.findMany({
+//       where: {
+//        //tbd
+//       },
+//       select: {
+//         customerPhone: true, 
+//       },
+//     });
+
+//     const numbers = orders.map(order => order.customerPhone).filter(Boolean).join(','); 
+
+//     if (!numbers) {
+//       return NextResponse.json({ message: "No phone numbers found" }, { status: 404 });
+//     }
+
+    const url = `https://api.textlocal.in/send/?apiKey=${encodeURIComponent(apiKey)}&numbers=${encodeURIComponent(numbers)}&sender=${encodeURIComponent(sender)}&message=${encodeURIComponent(message)}`;
+
+    const response = await fetch(url, { method: "POST" });
 
     if (!response.ok) {
       throw new Error("Failed to send SMS");
@@ -23,5 +40,8 @@ export async function GET() {
   } catch (error) {
     console.error("Error sending SMS:", error);
     return NextResponse.json({ message: "Failed to send SMS", error: error.message }, { status: 500 });
-  }
+  } 
+//   finally {
+//     await prisma.$disconnect();
+//   }
 }
